892ee92d7125558fde8e3619a9c4126ddeecdec5
oleh-hud-nure <oleh.hud@nure.ua>
2025-08-07 16:42:58 +0300

    LMS-17 - Added user model

--- src/features/auth/router.ts
+++ src/features/auth/router.ts
@@ -1,12 +1,22 @@
 import express, { Router } from 'express';
+import { User } from 'shared/models/User';
 
 const authRouter = Router()
 
-authRouter.get('/me', async function (req, res, next) {
-    res.json({
-        id: 'id',
-        name: 'Surname'
+authRouter.post('/register', async function (req, res, next) {
+    // TODO: Add the correct implementation
+    const newUser = new User.Model({
+        name: 'First',
+        email: 'email@email.com'
     })
+    const createdUser = await newUser.save()
+    res.status(200).json(createdUser as User.IBase)
+})
+
+authRouter.get('/me', async function (req, res, next) {
+    // TODO: Add the correct implementation
+    // const user = await User.Model.findById('')
+    // res.status(200).json(user as User.IBase)
 })
 
 export default authRouter
